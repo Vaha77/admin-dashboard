5fee857ae9572010ce00d4c0b67f639fcfe6e79a
vaha77 <abdulvohid996@gmail.com>
2022-05-24 14:34:26 +0900

    js

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Buuton = () => {
-  return <div>Buuton</div>;
+const Buuton = ({ color, bgColor, text, size, borderRadius }) => {
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Buuton;
